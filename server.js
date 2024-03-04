b2da9db97691dbb208ce8a187a3da4bb0f75ebed
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2024-03-04 12:25:38 +0100

    feat: game principal working

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const gameState = createGameState();
 const rooms = [];
 const connectedUsers = {};
 
+let playersTurn = 1;
+
 function createGameState() {
   return [
     { index: 0, player: null },
@@ -25,13 +27,18 @@ function createGameState() {
     { index: 6, player: null },
     { index: 7, player: null },
     { index: 8, player: null },
+   
   ];
 }
 
-function updateGameState(index) {
-  gameState[index].player = 1;
-  io.emit("game_state", gameState);
-  console.log(gameState);
+function updateGameState(data) {
+
+  gameState[data.move].player =data.player;
+  playersTurn = data.player === 1 ? 2 : 1;
+  io.to(data.roomID).emit("game_state", gameState);
+  io.to(data.roomID).emit("turn", playersTurn);
+  console.log("gameState", gameState);
+ 
 }
 
 // Enable CORS
@@ -45,25 +52,38 @@ const io = new Server(server, {
   },
 });
 
-io.engine.generateId = function (req) {
-  // generate a new custom id here
-  return 1;
-};
+
 
 let firstTime = true;
 let roomID;
+
 // a client has connected
 io.on("connection", (socket) => {
+
+  var currentRooms = io.sockets.adapter.rooms;
+  console.log("rooms", currentRooms);
+
   socket.on("join_room", (data) => {
-    //console.log("user wants to join here", data);
+    console.log("user wants to join here", data);
+    socket.join(data);
+    
+    io.to(data).emit("connected", {
+      connected: true,
+      roomID: data,
+      userID: socket.id,
+      player:2
+    });
+
+    io.to(data).emit("game_state", gameState);
+    io.to(data).emit("turn", 1);
   });
 
   socket.on("input", (input) => {
     console.log("input", input);
-    // ToDo: check if it is the users turn
-
+    console.log(currentRooms)
+    console.log("playersTurn", playersTurn);
     //if valid
-    updateGameState(input.move);
+    updateGameState(input);
 
     //console.log("user wants to join here", data);
   });
@@ -73,14 +93,18 @@ io.on("connection", (socket) => {
       connected: true,
       roomID: name + "-" + socket.id,
       userID: socket.id,
+      player:1
     });
 
     //console.log("room created with data:", name + "-" + socket.id);
     socket.join(name + "-" + socket.id);
-    // var currentRooms = io.sockets.adapter.rooms; // Change variable name to currentRooms
-    //console.log("rooms-join", currentRooms);
+  
+    console.log("rooms-join", currentRooms);
 
     io.to(name + "-" + socket.id).emit("game_state", gameState);
+    playersTurn = 1;
+    console.log("playersTurn", playersTurn);
+    io.to(name + "-" + socket.id).emit("turn", playersTurn);
   });
 
   // Handle user disconnection
